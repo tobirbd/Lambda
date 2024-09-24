fdb4741da3788995cc5b73fd494b55611894c39d
unknown <tobias.rebenda@aon.at>
2024-09-24 14:54:51 +0200

    normale for-each Schleife erstellt

--- LambdaHausuebung/src/HalloJavamitForEach.java
+++ LambdaHausuebung/src/HalloJavamitForEach.java
@@ -3,5 +3,9 @@ import java.util.List;
 public class HalloJavamitForEach {
     List<String> list;
 
-    
+    public void showList(){
+        for (String s : list){
+            System.out.println(list);
+        }
+    }
 }
