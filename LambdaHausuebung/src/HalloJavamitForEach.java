b78dea216e0dc1c9d44c1e4850eca0be56f6ec54
unknown <tobias.rebenda@aon.at>
2024-09-24 15:26:08 +0200

    for-each Methode des Interface List aufgerufen und erneut ausgegeben

--- LambdaHausuebung/src/HalloJavamitForEach.java
+++ LambdaHausuebung/src/HalloJavamitForEach.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.HexFormat;
 import java.util.List;
 
 public class HalloJavamitForEach {
     List<String> list;
+    public HalloJavamitForEach(){
+        this.list = new ArrayList<>();
+        list.add("Hallo");
+        list.add("Welt");
+    }
 
     public void showList(){
         for (String s : list){
-            System.out.println(list);
+            System.out.println(s);
         }
+
+        list.forEach((String s) -> System.out.println(s));
     }
 }
