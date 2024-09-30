ad9ff294f4dbed46210f0c7efe48efda3145dcbf
unknown <tobias.rebenda@aon.at>
2024-09-30 19:28:11 +0200

    Klasse NumberTester fertig und in main aufgerufen, hat funktioniert

--- LambdaHausuebung/src/Main.java
+++ LambdaHausuebung/src/Main.java
@@ -4,5 +4,26 @@ public class Main {
     public static void main(String[] args) {
         HalloJavamitForEach halloJavamitForEach = new HalloJavamitForEach();
         halloJavamitForEach.showList();
+
+        NumberTester tester = new NumberTester("filetxt.txt");
+
+        tester.setOddEvenTester(number -> number % 2 != 0);
+
+        tester.setPrimeTester(number -> {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) return false;
+            }
+            return true;
+        });
+
+        tester.setPalindromeTester(number -> {
+            String original = String.valueOf(number);
+            String reversed = new StringBuilder(original).reverse().toString();
+            return original.equals(reversed);
+        });
+
+        tester.testFile();
     }
+
 }
--- LambdaHausuebung/src/NumberTester.java
+++ LambdaHausuebung/src/NumberTester.java
@@ -1,9 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberTester {
     private NumberTest oddTester;
     private NumberTest primeTester;
     private NumberTest palindromeTester;
+    private List<int[]> testCases;
     public NumberTester(String fileName){
-
+        testCases = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            int numTestCases = Integer.parseInt(br.readLine());
+            for (int i = 0; i < numTestCases; i++) {
+                String[] parts = br.readLine().split(" ");
+                int testType = Integer.parseInt(parts[0]);
+                int number = Integer.parseInt(parts[1]);
+                testCases.add(new int[]{testType, number});
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
     public void setOddEvenTester(NumberTest oddTester){
         this.oddTester = oddTester;
@@ -17,6 +36,29 @@ public class NumberTester {
         this.palindromeTester = palindromeTester;
     }
     public void testFile(){
+        for (int[] testCase : testCases) {
+            int testType = testCase[0];
+            int number = testCase[1];
+
+            switch (testType) {
+                case 1:
+                    if (oddTester != null) {
+                        System.out.println(number + " is " + (oddTester.testNumber(number) ? "Odd" : "Even"));
+                    }
+                    break;
+                case 2:
+                    if (primeTester != null) {
+                        System.out.println(number + " is " + (primeTester.testNumber(number) ? "Prime" : "Not Prime"));
+                    }
+                    break;
+                case 3:
+                    if (palindromeTester != null) {
+                        System.out.println(number + " is " + (palindromeTester.testNumber(number) ? "Palindrome" : "Not Palindrome"));
+                    }
+                    break;
+                default: System.out.println("Unknown test type: " + testType);
+            }
+        }
 
     }
 }
